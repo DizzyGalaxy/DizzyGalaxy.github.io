1a617bde8edcdbb620d48e4accaf86cb7f940009
DizzyGalaxy <anna.devgirl428@gmail.com>
2025-07-29 18:48:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function gameDev() {
+    window.location.href = "gameDev.html"
+}
+
+function writings() {
+    window.location.href = "writings.html"
+}
+
+function stuffWorks() {
+    window.location.href = "stuffWorks.html"
+}
+
+function artworks() {
+    window.location.href = "artworks.html"
+}
+
+function goBack() {
+    window.location.href = "index.html"
+}
